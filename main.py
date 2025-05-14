df98e9498c07e8b3e139ffb65b60cb83bdda2c5b
Augustr-pro <BUS_ARhoda26@outlook.com>
2025-05-14 08:54:52 -0400

    CH2 L4

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ def get_book_text(filepath):
             file_contents = f.read()
         return file_contents
 
+def count_words(txt_file):
+    txt_vector = txt_file.split()
+    word_count = len(txt_vector)
+    return word_count
+
 def main():
     book_text = get_book_text('books/frankenstein.txt')
-    print(book_text)
+    print(f'{count_words(book_text)} words found in the document')
 
 main()
