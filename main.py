7ce52d5e099e5db76f646c2651e3f9a62cb08291
Augustr-pro <BUS_ARhoda26@outlook.com>
2025-05-14 09:20:35 -0400

    CH2 L6

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_characters
 
 def get_book_text(filepath):
         with open(filepath) as f:
@@ -8,5 +9,7 @@ def get_book_text(filepath):
 def main():
     book_text = get_book_text('books/frankenstein.txt')
     print(f'{count_words(book_text)} words found in the document')
+    char_count = count_characters(book_text)
+    print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,11 @@ def count_words(txt_file):
     txt_vector = txt_file.split()
     word_count = len(txt_vector)
     return word_count
+
+def count_characters(txt_file):
+    char_count = {}  # Initialize an empty dictionary
+
+    for char in txt_file.lower():  # Convert characters to lowercase
+        char_count[char] = char_count.get(char, 0) + 1  # Count occurrences
+    
+    return char_count  # Return the dictionary
